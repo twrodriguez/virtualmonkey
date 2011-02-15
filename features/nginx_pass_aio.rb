6c63e13abd5586064ff60aa1b283461eef4cc767
Tim Rodriguez <timothy@rightscale.com>
2011-02-15 18:59:00 +0000

    Added AIO plust start stop

---
+++ features/nginx_pass_aio.rb
@@ -0,0 +1,30 @@
+#@lamp_test
+#
+#Feature: Nginx Passenger AIO Server Template Test
+#  Tests the deployment
+#
+#Scenario: Nginx Passenger AIO Server Template Test
+#
+# Given A AIO deployment
+  @runner = VirtualMonkey::LampRunner.new(ENV['DEPLOYMENT'])
+
+# Then I should stop the servers
+  @runner.behavior(:stop_all)
+
+# Then I should launch all servers
+  @runner.behavior(:launch_all)
+
+# Then I should wait for the state of "all" servers to be "operational"
+  @runner.behavior(:wait_for_all, "operational")
+
+# Then I should run LAMP checks
+  @runner.behavior(:run_lamp_checks)
+
+# Then I should run mysql checks
+  @runner.behavior(:run_checks)
+
+# Then I should check that ulimit was set correctly
+  @runner.probe(".*", "su - mysql -s /bin/bash -c \"ulimit -n\"") { |s| s.to_i > 1024 }
+
+# Then I should check that monitoring is enabled
+  @runner.behavior(:check_monitoring)
