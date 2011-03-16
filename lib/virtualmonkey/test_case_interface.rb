c946858c7fd0384053c0677ddb64522a8c4b72ae
Tim Rodriguez <timothy@rightscale.com>
2011-03-16 14:29:42 -0700

    Merged Behavior and Verify functions. Upgraded Probe. Backwards-Compat

--- lib/virtualmonkey/test_case_interface.rb
+++ lib/virtualmonkey/test_case_interface.rb
@@ -4,22 +4,30 @@ module VirtualMonkey
       behavior(sym, *args)
     end
 
-    def behavior(sym, *args)
+    def behavior(sym, *args, &block)
       begin
         rerun_test
         #pre-command
         populate_settings unless @populated
         #command
         result = __send__(sym, *args)
+        if block
+          raise "FATAL: Failed behavior verification. Result was:\n#{result.inspect}" if not yield(result)
+        end
         #post-command
         continue_test
       rescue Exception => e
-        dev_mode?(e)
+        if block and e.message !~ /^FATAL: Failed behavior verification/
+          dev_mode?(e) if not yield(e)
+        else
+          dev_mode?(e)
+        end
       end while @rerun_last_command.pop
       result
     end
 
     def verify(method, expectation, *args)
+      puts "TestCaseInterface::verify is deprecated!"
       if expectation =~ /((exception)|(error)|(fatal)|(fail))/i
         expect = "fail"
         error_msg = expectation.split(":")[1..-1].join(":")
@@ -33,7 +41,7 @@ module VirtualMonkey
 
       begin
         rerun_test
-        result = __send__(command, *args)
+        result = __send__(method, *args)
         if expect != "pass" and not (result == nil and expect == "nil")
           raise "FATAL: Failed verification"
         end
@@ -45,10 +53,10 @@ module VirtualMonkey
       end while @rerun_last_command.pop
     end
 
-    def probe(server, command, &block)
-      # run command on servers matching "server" over ssh
+    def probe(set, command, &block)
+      # run command on set over ssh
       result = ""
-      @servers.select { |s| s.nickname =~ /#{server}/ }.each { |s|
+      select_set(set).each { |s|
         begin
           rerun_test
           result_temp = s.spot_check_command(command)
@@ -91,32 +99,26 @@ module VirtualMonkey
 
     def help
       puts "Here are some of the wrapper methods that may be of use to you in your debugging quest:\n"
-      puts "behavior(sym, *args): Pass the method name (as a symbol or string) and the optional arguments"
-      puts "                      that you wish to pass to that method; behavior() will call that method"
-      puts "                      with those arguments while handling nested exceptions, retries, and"
-      puts "                      debugger calls.\n"
-      puts "                      Examples:"
-      puts "                        behavior(:launch_all)"
-      puts "                        behavior(:launch_set, 'Load Balancer')\n"
-      puts "verify(sym, expectation_string, *args): Pass the method name (as a symbol or string), the expected" 
-      puts "                                        result, and any arguments to pass to that method. The"
-      puts "                                        expectation_string should consist of 'Error: MyRegex',"
-      puts "                                        'Pass', or 'nil'. 'Error: MyRegex' tells verify() that it"
-      puts "                                        should expect an exception to be raised, and the message "
-      puts "                                        or exception name should match /MyRegex/. 'Pass' tells"
-      puts "                                        verify() that it should expect the method to return normally,"
-      puts "                                        and 'nil' tells verify() that it should expect the method to"
-      puts "                                        return nil.\n"
-      puts "                                        Example:"
-      puts "                                          verify(:launch_all, 'Error: execution expired')\n"
-      puts "probe(server_regex, shell_command, &block): Provides a one-line interface for running a command on"
-      puts "                                            a set of servers and verifying their output. The block"
-      puts "                                            should take one argument, the output string from one of"
-      puts "                                            the servers, and return true or false based on however"
-      puts "                                            the developer wants to verify correctness.\n"
-      puts "                                            Examples:"
-      puts "                                              probe('.*', 'ls') { |s| puts s }"
-      puts "                                              probe('.*', 'uname -a') { |s| s =~ /x64/ }\n"
+      puts "behavior(sym, *args, &block): Pass the method name (as a symbol or string) and the optional arguments"
+      puts "                              that you wish to pass to that method; behavior() will call that method"
+      puts "                              with those arguments while handling nested exceptions, retries, and"
+      puts "                              debugger calls. If a block is passed, it should take one argument, the"
+      puts "                              return value of the function 'sym'. The block should always check"
+      puts "                              if the return value is an Exception or not, and validate accordingly.\n"
+      puts "                              Examples:"
+      puts "                                behavior(:launch_all)"
+      puts "                                behavior(:launch_set, 'Load Balancer')"
+      puts "                                behavior(:run_script_on_all, 'fail') { |r| r.is_a?(Exception) }\n"
+      puts "probe(server_set, shell_command, &block): Provides a one-line interface for running a command on"
+      puts "                                          a set of servers and verifying their output. The block"
+      puts "                                          should take one argument, the output string from one of"
+      puts "                                          the servers, and return true or false based on however"
+      puts "                                          the developer wants to verify correctness.\n"
+      puts "                                          Examples:"
+      puts "                                            probe('.*', 'ls') { |s| puts s }"
+      puts "                                            probe(:fe_servers, 'ls') { |s| puts s }"
+      puts "                                            probe('app_servers', 'ls') { |s| puts s }"
+      puts "                                            probe('.*', 'uname -a') { |s| s =~ /x64/ }\n"
       puts "continue_test: Disables the retry loop that reruns the last command (the current command that you're"
       puts "               debugging.\n"
       puts "help: Prints this help message."
@@ -128,7 +130,20 @@ module VirtualMonkey
       @populated = 1
     end
 
-    def object_behavior(obj, sym, *args)
+    def select_set(set = @servers)
+      if set.is_a?(String)
+        if self.respond_to?(set.to_sym)
+          set = set.to_sym
+        else
+          set = @servers.select { |s| s.nickname =~ /#{set}/ }
+        end
+      end
+      set = behavior(set) if set.is_a?(Symbol)
+      set = [ set ] unless set.is_a?(Array)
+      return set
+    end
+
+    def object_behavior(obj, sym, *args, &block)
       begin
         rerun_test
         #pre-command
