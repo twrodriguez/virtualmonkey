ee1394e43636fb852add69e40458f162477464d3
Erik Kargard <erik@rightscale.com>
2011-03-19 00:26:29 +0000

    Passenger monitoring tests

--- features/nginx_pass_aio.rb
+++ features/nginx_pass_aio.rb
@@ -28,3 +28,4 @@
 
 # Then I should check that monitoring is enabled
   @runner.behavior(:check_monitoring)
+  @runner.behavior(:check_passenger_monitoring
--- lib/virtualmonkey/onboarding_runner.rb
+++ lib/virtualmonkey/onboarding_runner.rb
@@ -24,6 +24,41 @@ module VirtualMonkey
       end
     end
 
-
+# Check for specific passenger data.
+    def check_passenger_monitoring
+      passenger_plugins = [
+                        {"plugin_name"=>"passenger", "plugin_type"=>"passenger_instances","field"=>"value"},
+                        {"plugin_name"=>"passenger", "plugin_type"=>"passenger_processes","field"=>"max"},
+                        {"plugin_name"=>"passenger", "plugin_type"=>"passenger_queued","field"=>"value"},
+                        {"plugin_name"=>"passenger", "plugin_type"=>"passenger_requests","field"=>"value"}
+                      ]
+      sleep 60 # wait for some data to be available
+      @servers.each do |server|
+        unless server.multicloud
+#passenger commands to generate data for collectd to return
+#          for ii in 1...100
+#            # how do we force there to be data??  For now just check that the graph exists - cause the
+#            # bug was missing graphs.
+#          end
+          passenger_plugins.each do |plugin|
+            monitor = server.get_sketchy_data({'start' => -60,
+                                               'end' => -20,
+                                               'plugin_name' => plugin['plugin_name'],
+                                               'plugin_type' => plugin['plugin_type']})
+            value = monitor['data']["#{plugin['field']}"]
+            puts "Checking #{plugin['plugin_name']}-#{plugin['plugin_type']}: value #{value}"
+            raise "No #{plugin['plugin_name']}-#{plugin['plugin_type']} data" unless value.length > 0
+#            # Need to check for that there is at least one non 0 value returned.
+#            for nn in 0...value.length
+#              if value[nn] > 0
+#                break
+#              end
+#            end
+#            raise "No #{plugin['plugin_name']}-#{plugin['plugin_type']} time" unless nn < value.length
+            puts "Monitoring is OK for #{plugin['plugin_name']}-#{plugin['plugin_type']}"
+          end
+        end
+      end
+    end
   end
 end
