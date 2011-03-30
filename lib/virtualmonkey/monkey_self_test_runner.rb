3fc1c9d48d45b9a527715e796f2494c773033a7d
Tim Rodriguez <timothy@rightscale.com>
2011-03-30 16:53:31 -0700

    bugfix

---
+++ lib/virtualmonkey/monkey_self_test_runner.rb
@@ -0,0 +1,14 @@
+module VirtualMonkey
+  class SimpleRunner
+    include VirtualMonkey::DeploymentRunner
+    include VirtualMonkey::Simple
+
+    def lookup_scripts
+     scripts = [
+                 [ 'test', 'exit value' ]
+               ]
+      st = ServerTemplate.find(resource_id(s_one.server_template_href))
+      lookup_scripts_table(st,scripts)
+    end
+  end
+end
