4aa7dd341b4148ff43997b95f0edc73f2e89b713
luke singh <lukesingh@rightscale.com>
2011-08-03 21:35:19 +0000

    ADD rightlink virtual monkey files

---
+++ features/rightlink.rb
@@ -0,0 +1,22 @@
+set :runner, VirtualMonkey::Runner::Rightlink
+
+clean_start do
+  @runner.stop_all
+end
+
+before do
+  @runner.launch_all
+  @runner.wait_for_all("operational")
+end
+
+test "default" do
+  @runner.wait_for_all("operational")
+  @runner.test_state_test_check
+  @runner.test_remote_recipe_test
+  @runner.test_remote_recipe_ping
+  @runner.test_resource_remote_pong
+  @runner.test_persist_test_check
+  @runner.test_depend_check
+  @runner.test_iteration_check
+  @runner.test_print_inputs
+end
---
+++ lib/virtualmonkey/deployment_runners/rightlink_runner.rb
@@ -0,0 +1,10 @@
+module VirtualMonkey
+  module Runner
+    class Rightlink
+      include VirtualMonkey::Mixin::DeploymentBase
+      include VirtualMonkey::Mixin::Rightlink
+
+      # Override any functions from mixins here
+    end
+  end
+end
