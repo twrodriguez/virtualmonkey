d7a3295fc3bc895239ba9924b59525d66202db6a
Douglas Thrit <douglas.thrift@rightscale.com>
2011-08-04 22:11:15 +0000

    WIP

--- features/php_fullstack_awsdns.rb
+++ features/php_fullstack_awsdns.rb
@@ -21,6 +21,7 @@ before do
   @runner.launch_set(:fe_servers)
   @runner.wait_for_set(:mysql_servers, "operational")
   @runner.set_private_mysql_fqdn
+CHANGE TO setup_chef_dns
   @runner.import_unified_app_sqldump
   @runner.wait_for_set(:fe_servers, "operational")
   @runner.launch_set(:app_servers)
@@ -32,74 +33,7 @@ end
 ## Unified Application on 8000
 #
 
-#test "run_unified_application_checks" do
-#  @runner.run_unified_application_checks(:app_servers, 8000)
-#end
-
-test "reboot_operations" do
-  @runner.run_reboot_operations
+test "run_unified_application_checks" do
+  @runner.run_unified_application_checks(:app_servers, 8000)
 end
 
-test "monitoring" do
-  @runner.check_monitoring
-end
-
-#
-## ATTACHMENT GROUP
-#
-
-test "attach_all" do
-  @runner.test_attach_all
-  @runner.frontend_checks(80)
-end
-
-test "attach_request" do
-  @runner.test_attach_request
-  @runner.frontend_checks(80)
-end
-
-after "attach_all", "attach_request" do
-  @runner.test_detach
-end
-
-#
-## Reconverge Test
-#
-
-#before "reconverge" do
- # @runner.enable_reconverge
- # @runner.set_variation_cron_time
-#end
-
-#test "reconverge" do
-#  @runner.detach_all
-#  puts sleep(60*2) # 2 minutes
-#  @runner.frontend_checks(80)
-#end
-
-#before "cron_reconverge" do
-#  @runner.enable_reconverge
-#end
-
-#test "cron_reconverge" do
- # @runner.test_cron_reconverge    ###### not doing this anymore so dont test it
-#end
-
-#after "reconverge", "cron_reconverge" do
- # @runner.disable_reconverge
-#end
-
-#
-## Defunct Server Test
-#
-
-before "defunct_server" do
-  @runner.set_variation_defunct_server
-end
-
-test "defunct_server" do
-  @runner.transaction do
-    @runner.test_attach_all
-    @runner.test_defunct_server
-  end
-end
