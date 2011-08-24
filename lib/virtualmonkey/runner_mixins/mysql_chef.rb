ac264be2eb796f5f73cd0e8edec2ab04ecb265e9
caryp <cary@rightscale.com>
2011-08-24 13:23:10 -0700

    [mysql_chef] updated inputs after mysql refactor

--- lib/virtualmonkey/runner_mixins/mysql_chef.rb
+++ lib/virtualmonkey/runner_mixins/mysql_chef.rb
@@ -96,16 +96,16 @@ module VirtualMonkey
       end
   
       def test_ebs
-       run_script("setup_block_device", s_one)
+        run_script("setup_block_device", s_one)
         probe(s_one, "touch /mnt/storage/monkey_was_here")
-        sleep 100
-       run_script("do_backup", s_one)
+        #sleep 100
+        run_script("do_backup", s_one)
         wait_for_snapshots
-        sleep 100
-       run_script("do_force_reset", s_one)
-  # need to wait here for the volume status to settle (detaching)
-        sleep 400
-       run_script("do_restore", s_one)
+        #sleep 100
+        run_script("do_force_reset", s_one)
+        # need to wait here for the volume status to settle (detaching)
+        #sleep 400
+        run_script("do_restore", s_one)
         probe(s_one, "ls /mnt/storage") do |result, status|
           raise "FATAL: no files found in the backup" if result == nil || result.empty?
           true
