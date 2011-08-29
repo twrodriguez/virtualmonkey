581dec2da855a886a27bd96ac7e60386a0cddb36
luke singh <lukesingh@rightscale.com>
2011-08-29 22:46:35 +0000

    wishbone changes to reboot wishbone in a certain order and not reboot the database_server

--- lib/virtualmonkey/runner_mixins/wishbone.rb
+++ lib/virtualmonkey/runner_mixins/wishbone.rb
@@ -62,10 +62,22 @@ module VirtualMonkey
       def run_reboot_operations
   # Duplicate code here because we need to wait between the master and the slave time
         #reboot_all(true) # serially_reboot = true
-        @servers.each do |s|
-          obj_behavior(s, :reboot, true)
-          obj_behavior(s, :wait_for_state, "operational")
-        end
+       # @servers.each do |s|
+ #        transaction { mysql_servers.first.reboot( true)}
+  #       transaction {mysql_servers.first.wait_for_state( "operational") }
+         
+         transaction { fe_servers[0].reboot( true)}
+         transaction { fe_servers[0].wait_for_state( "operational")}
+         
+         transaction { fe_servers[1].reboot( true)}
+         transaction { fe_servers[1].wait_for_state( "operational")}
+         
+         transaction {app_servers[0].reboot( true) }
+         transaction {app_servers[0].wait_for_state( "operational") }
+         
+         transaction {app_servers[1].reboot( true) }
+         transaction {app_servers[1].wait_for_state( "operational") }
+        #end
        wait_for_all("operational")
        run_reboot_checks
       end
@@ -73,12 +85,24 @@ module VirtualMonkey
       # This is where we perform multiple checks on the deployment after a reboot.
       def run_reboot_checks
         # one simple check we can do is the backup.  Backup can fail if anything is amiss
-	setup_block_device
+       sleep(60)
+       run_unified_application_checks(fe_servers, 80)
+        recipes = [
+                    [ 'iptable_rules', 'sys_firewall::do_list_rules' ]
+                  ]
+        mysql_st = ServerTemplate.find(resource_id(mysql_servers.first.server_template_href))
+        load_script_table(mysql_st,recipes)
+        
+       run_script_on_set('iptable_rules', mysql_servers.first)
+
        run_script("do_backup", mysql_servers.first)
        sleep(120)
        run_unified_application_checks(fe_servers, 80)
       end
 
+      def setup_block_device
+        run_script("setup_block_device", mysql_servers.first)
+      end
     end
   end
 end
