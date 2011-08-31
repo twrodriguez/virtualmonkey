064efa50eb74082e1e8fd88adb628df771e602fe
Lopaka Delp <lopaka@rightscale.com>
2011-08-31 13:57:27 -0700

    removed set_variation_storage_type from features/php_suite.rb and php_suit_ssl.rb

--- features/php_suite.rb
+++ features/php_suite.rb
@@ -7,8 +7,6 @@ end
 before do
 # PHP/FE variations
 
-# TODO: variations to set
-# mysql fqdn
  @runner.setup_dns("dnsmadeeasy_new") # dnsmadeeasy
 @runner.set_variation_dnschoice("text:DNSMadeEasy") # set variation choice
   @runner.set_variation_http_only
@@ -16,7 +14,6 @@ before do
 # Mysql variations
   @runner.set_variation_lineage
   @runner.set_variation_container
-  @runner.set_variation_storage_type
 
 # launching
   @runner.launch_set(:mysql_servers)
