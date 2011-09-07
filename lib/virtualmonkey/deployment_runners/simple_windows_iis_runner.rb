50ea16d15eb290e8b0eb8d9b30611ae38a0e7263
shawnduex <shawn@rightscale.com>
2011-09-07 13:32:34 -0700

    Edited lib/virtualmonkey/deployment_runners/simple_windows_iis_runner.rb via GitHub

--- lib/virtualmonkey/deployment_runners/simple_windows_iis_runner.rb
+++ lib/virtualmonkey/deployment_runners/simple_windows_iis_runner.rb
@@ -25,11 +25,11 @@ module VirtualMonkey
                  ]
         st = @server_templates.detect{ |st| st.nickname =~ /Microsoft IIS App/i } 
         load_script_table(st,scripts)
-        load_script('SYS Install Web Deploy 2.0 check', RightScript.new('href' => "/api/acct//2901/right_scripts/434985"))
-        load_script('IIS Restart web server check', RightScript.new('href' => "/api/acct//2901/right_scripts/435028"))
-        load_script('IIS monkey tests', RightScript.new('href' => "/api/acct//2901/right_scripts/435044"))
-        load_script('SYS install ASP.NET MVC 3 check', RightScript.new('href' => "/api/acct//2901/right_scripts/434989"))
-        load_script('SYS Install .NET Framework 4 check', RightScript.new('href' => "/api/acct//2901/right_scripts/434993"))
+        load_script('SYS Install Web Deploy 2.0 check', RightScript.new('href' => "/api/acct/2901/right_scripts/434985"))
+        load_script('IIS Restart web server check', RightScript.new('href' => "/api/acct/2901/right_scripts/435028"))
+        load_script('IIS monkey tests', RightScript.new('href' => "/api/acct/2901/right_scripts/435044"))
+        load_script('SYS install ASP.NET MVC 3 check', RightScript.new('href' => "/api/acct/2901/right_scripts/434989"))
+        load_script('SYS Install .NET Framework 4 check', RightScript.new('href' => "/api/acct/2901/right_scripts/434993"))
       end
     end
   end
