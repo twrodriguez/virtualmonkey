baa746538a26760301ee2dfcd079cd08f5f1f49e
Tim Rodriguez <timothy@rightscale.com>
2011-09-07 11:08:29 -0700

    bugfix

--- lib/virtualmonkey/deployment_runners/simple_windows_iis_runner.rb
+++ lib/virtualmonkey/deployment_runners/simple_windows_iis_runner.rb
@@ -7,7 +7,7 @@ module VirtualMonkey
       def server_iis
         #  @servers.select { |s| s.nickname =~ /Microsoft IIS App/i }.first
         st = @server_templates.detect{ |st| st.nickname =~ /Microsoft IIS App/i } 
-        match_servers_by_st(st)
+        match_servers_by_st(st).first
       end
   
       def oleg_windows_iis_lookup_scripts
@@ -23,7 +23,7 @@ module VirtualMonkey
                    [ 'SYS Install .NET Framework 4', 'SYS Install .NET Framework 4' ],
                    [ 'SYS install ASP.NET MVC 3', 'SYS install ASP.NET MVC 3' ],
                  ]
-        st = ServerTemplate.find(resource_id(server_iis.server_template_href))
+        st = @server_templates.detect{ |st| st.nickname =~ /Microsoft IIS App/i } 
         load_script_table(st,scripts)
         load_script('SYS Install Web Deploy 2.0 check', RightScript.new('href' => "/api/acct//2901/right_scripts/434985"))
         load_script('IIS Restart web server check', RightScript.new('href' => "/api/acct//2901/right_scripts/435028"))
