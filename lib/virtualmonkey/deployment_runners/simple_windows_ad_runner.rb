861b212ade2a0dcf2845b6ff662377b523018642
Oleg Sviryd <oleg.sviryd@altoros.com>
2011-09-13 16:54:48 +0300

    fixed AD ST on publish account

--- lib/virtualmonkey/deployment_runners/simple_windows_ad_runner.rb
+++ lib/virtualmonkey/deployment_runners/simple_windows_ad_runner.rb
@@ -10,29 +10,29 @@ module VirtualMonkey
   
       def oleg_windows_ad_lookup_scripts
        scripts = [
-                   [ 'AD change Administrator password', 'AD change Administrator password' ],
-                   [ 'AD create a backup', 'AD create a backup' ],
-                   [ 'AD restore from backup', 'AD restore from backup' ],
-                   [ 'AD create a new group', 'AD create a new group' ],
-                   [ 'AD create a new user', 'AD create a new user' ],
-                   [ 'AD bulk add user', 'AD bulk add user' ],
-                   [ 'AD recreate domain shares', 'AD recreate domain shares' ],
-                   [ 'AD install ADFS', 'AD install ADFS' ],
-                   [ 'AD Transferring FSMO Roles', 'AD Transferring FSMO Roles' ],
-                   [ 'SYS change to safe boot mode', 'SYS change to safe boot mode' ],
-                   [ 'SYS change to normal boot mode', 'SYS change to normal boot mode' ],
-                   [ 'SYS Install AD Backup Policy', 'SYS Install AD Backup Policy' ],
+                   [ 'AD Create a new user', 'AD Create a new user' ],
+                   [ 'AD Create a new group', 'AD Create a new group' ],
+                   [ 'AD Create system state backup', 'AD Create system state backup' ],
+                   [ 'AD Restore from backup', 'AD Restore from backup' ],
+                   [ 'AD Transferring FSMO roles', 'AD Transferring FSMO roles' ],
+                   [ 'AD Bulk create new user', 'AD Bulk create new user' ],
+                   [ 'AD Change Administrator password', 'AD Change Administrator password' ],
+                   [ 'AD Rebuild domain shares', 'AD Rebuild domain shares' ],
+                   [ 'SYS Change to safe boot mode', 'SYS Change to safe boot mode' ],
+                   [ 'SYS Change to normal boot mode', 'SYS Change to normal boot mode' ],
+                   [ 'SYS Install AD backup policy', 'SYS Install AD backup policy' ],
+                   [ 'AD Install ADFS', 'AD Install ADFS' ],
                  ]
         st = ServerTemplate.find(resource_id(server_ad.server_template_href))
         load_script_table(st,scripts)
-        load_script('AD monkey test', RightScript.new('href' => "/api/acct/2901/right_scripts/416272"))
-        load_script('SYS Install AD Backup Policy CHECK', RightScript.new('href' => "/api/acct/2901/right_scripts/417111"))
-        load_script('AD create a new user CHECK', RightScript.new('href' => "/api/acct/2901/right_scripts/417115"))
-        load_script('AD create a new group CHECK', RightScript.new('href' => "/api/acct/2901/right_scripts/417117"))
-        load_script('AD bulk add user CHECK', RightScript.new('href' => "/api/acct/2901/right_scripts/417157"))
-        load_script('AD install ADFS CHECK', RightScript.new('href' => "/api/acct/2901/right_scripts/418000"))
-        load_script('SYS change to safe boot mode CHECK', RightScript.new('href' => "/api/acct/2901/right_scripts/417109"))
-        load_script('SYS change to normal boot mode CHECK', RightScript.new('href' => "/api/acct/2901/right_scripts/417110"))
+        load_script('AD monkey test', RightScript.new('href' => "/api/acct/2901/right_scripts/438784"))
+        load_script('SYS Install AD Backup Policy CHECK', RightScript.new('href' => "/api/acct/2901/right_scripts/"))
+        load_script('AD create a new user CHECK', RightScript.new('href' => "/api/acct/2901/right_scripts/438800"))
+        load_script('AD create a new group CHECK', RightScript.new('href' => "/api/acct/2901/right_scripts/438798"))
+        load_script('AD bulk add user CHECK', RightScript.new('href' => "/api/acct/2901/right_scripts/438797"))
+        load_script('AD install ADFS CHECK', RightScript.new('href' => "/api/acct/2901/right_scripts/438869"))
+        load_script('SYS change to safe boot mode CHECK', RightScript.new('href' => "/api/acct/2901/right_scripts/438905"))
+        load_script('SYS change to normal boot mode CHECK', RightScript.new('href' => "/api/acct/2901/right_scripts/438874"))
       end
     end
   end
