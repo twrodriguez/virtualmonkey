5f4c3d54e2d5d6fea807d7b763bcccdd05b6dc9b
luke singh <lukesingh@rightscale.com>
2011-09-14 21:21:46 +0000

    whitespace changes

--- lib/virtualmonkey/runner_mixins/deployment_base.rb
+++ lib/virtualmonkey/runner_mixins/deployment_base.rb
@@ -4,7 +4,7 @@ module VirtualMonkey
       include VirtualMonkey::TestCaseInterface
       attr_accessor :deployment, :servers, :server_templates
       attr_accessor :scripts_to_run
-      
+
       def initialize(deployment, opts = {})
         @scripts_to_run = {}
         @server_templates = []
@@ -41,14 +41,14 @@ module VirtualMonkey
           ["/var/log/messages", ".*", "BEGIN RSA PRIVATE KEY"]
         ]
       end
-  
+
       # Ensures the following whitelist entries are entered for all runners
       def deployment_base_whitelist
         [
           ["/var/log/messages", ".*", "destination\(d_httperror\)=0"]
         ]
       end
-  
+
       # Makes this exception_handle available for all runners
       def deployment_base_exception_handle(e)
         if e.message =~ /Insufficient capacity/
@@ -67,14 +67,14 @@ module VirtualMonkey
           return false # Exception Not Handled
         end
       end
-  
+
       # Master method that calls all methods with "lookup_scripts" in their name
       def __lookup_scripts__
         all_methods = self.methods + self.private_methods
         lookup_script_methods = all_methods.select { |m| m =~ /lookup_scripts/ and m !~ /^__/ }
         lookup_script_methods.each { |method_name| self.__send__(method_name) }
       end
-  
+
       # Returns the API 1.0 integer id of the rest_connection object or href
       def resource_id(res)
         if res.is_a?(String)
@@ -83,7 +83,7 @@ module VirtualMonkey
           return res.href.split(/\//).last.to_i
         end
       end
-  
+
       # Loads a table of [friendly_name, script/recipe regex] from reference_template, attaching them to all templates in the deployment unless add_only_to_this_st is set
       def load_script_table(reference_template, table, add_only_to_this_st = nil)
         if add_only_to_this_st.is_a?(ServerInterface) or add_only_to_this_st.is_a?(Server)
@@ -103,7 +103,7 @@ module VirtualMonkey
           }
         }
       end
-  
+
       # Determines which logs to run for the available server_templates, then does the message check
       def run_logger_audit(interactive = false, strict = false)
         ret_string = ""
@@ -119,7 +119,7 @@ module VirtualMonkey
           return ret_string
         end
       end
-  
+
       # Loads a single hard-coded RightScript or Recipe, attaching it to all templates in the deployment unless add_only_to_this_st is set
       def load_script(friendly_name, script, add_only_to_this_st=nil)
         if add_only_to_this_st.is_a?(ServerInterface) or add_only_to_this_st.is_a?(Server)
@@ -138,7 +138,7 @@ module VirtualMonkey
             script_ref = RightScript.find(script)
           rescue
             script_ref = Executable.find(script)
-          end 
+          end
         elsif script.is_a?(String)
           script_ref = RightScript.find_by(:nickname) { |n| n =~ /#{Regexp.escape(script)}/i }.first
         end
@@ -149,23 +149,23 @@ module VirtualMonkey
           @scripts_to_run[resource_id(st)][friendly_name] = script_ref
         }
       end
-  
+
       def s_one
         @servers[0]
       end
-  
+
       def s_two
         @servers[1]
       end
-  
+
       def s_three
         @servers[2]
       end
-  
+
       def s_four
         @servers[3]
       end
-  
+
       # Launch all servers in the deployment.
       def launch_all
         @servers.each { |s|
@@ -176,20 +176,20 @@ module VirtualMonkey
           end
         }
       end
-  
+
       # sets the MASTER_DB_DNSNAME to this machine's ip address
       def set_master_db_dnsname
         the_name = get_tester_ip_addr
-        @deployment.set_input("MASTER_DB_DNSNAME", the_name) 
-        @deployment.set_input("DB_HOST_NAME", the_name) 
+        @deployment.set_input("MASTER_DB_DNSNAME", the_name)
+        @deployment.set_input("DB_HOST_NAME", the_name)
         @deployment.set_input("db_mysql/fqdn", the_name)
         @deployment.set_input("db/fqdn", the_name)
       end
-  
+
       # Launch server(s) that match nickname_substr
       # * nickname_substr<~String> - regex compatible string to match
       def launch_set(nickname_substr)
-        set = select_set(nickname_substr)  
+        set = select_set(nickname_substr)
         set.each { |s|
           begin
             transaction { s.start }
@@ -198,7 +198,7 @@ module VirtualMonkey
           end
         }
       end
-  
+
       # Re-Launch all server
       def relaunch_all
         @servers.each { |s|
@@ -209,7 +209,7 @@ module VirtualMonkey
           end
         }
       end
-  
+
       # un-set all tags on all servers in the deployment
       def unset_all_tags
         @servers.each do |s|
@@ -218,15 +218,15 @@ module VirtualMonkey
           transaction { s.save }
         end
       end
-  
+
       # Wait for server(s) matching nickname_substr to enter state
       # * nickname_substr<~String> - regex compatible string to match
       # * state<~String> - state to wait for, eg. operational
       def wait_for_set(nickname_substr, state, timeout=1200)
-        set = select_set(nickname_substr)  
+        set = select_set(nickname_substr)
         state_wait(set, state, timeout)
       end
-  
+
       # Helper method, waits for state on a set of servers.
       # * set<~Array> of servers to operate on
       # * state<~String> state to wait for
@@ -238,59 +238,59 @@ module VirtualMonkey
           set.each { |server| transaction { server.wait_for_state(state, timeout) } }
         end
       end
-      
+
       # Wait for all server(s) to enter state.
       # * state<~String> - state to wait for, eg. operational
       def wait_for_all(state, timeout=1200)
         state_wait(@servers, state, timeout)
       end
-  
+
       def start_ebs_all(wait=true)
         @servers.each { |s| transaction { s.start_ebs } }
         wait_for_all("operational") if wait
-        @servers.each { |s| 
-          s.dns_name = nil 
+        @servers.each { |s|
+          s.dns_name = nil
           s.private_dns_name = nil
         }
       end
-  
+
       def stop_ebs_all(wait=true)
         @servers.each { |s| transaction { s.stop_ebs } }
         wait_for_all("stopped") if wait
-        @servers.each { |s| 
-          s.dns_name = nil 
+        @servers.each { |s|
+          s.dns_name = nil
           s.private_dns_name = nil
         }
       end
-  
+
       def stop_all(wait=true)
         @servers.each { |s| s.stop }
         wait_for_all("stopped") if wait
-        @servers.each { |s| 
-          s.dns_name = nil 
+        @servers.each { |s|
+          s.dns_name = nil
           s.private_dns_name = nil
         }
       end
-  
+
       def reboot_all(serially_reboot = false)
         wait_for_reboot = true
         # Do NOT thread this each statement
-        @servers.each do |s| 
+        @servers.each do |s|
           transaction { s.reboot(wait_for_reboot) }
           if serially_reboot
             transaction { s.wait_for_state("operational") }
           end
         end
-        @servers.each do |s| 
+        @servers.each do |s|
           transaction { s.wait_for_state("operational") }
         end
       end
-  
+
       # Run a script on all servers in the deployment in parallel
       def run_script_on_all(friendly_name, wait = true, options = nil)
         run_script_on_set(friendly_name, @servers, wait, options)
       end
-  
+
       # Run a script on a set of servers in the deployment in parallel
       # * friendly_name<~String> = the hash key name of the desired script to run
       # * set can be any way of denoting a set of servers to run on:
@@ -305,7 +305,7 @@ module VirtualMonkey
       #                  audits for each
       # * options<~Hash> will pass specific inputs to the script to run with
       def run_script_on_set(friendly_name, set = @servers, wait = true, options = nil)
-        audits = Array.new() 
+        audits = Array.new()
         set = select_set(set)
         if wait
           set.each do |s|
@@ -332,25 +332,25 @@ module VirtualMonkey
           end
         end
       end
-  
+
       # Run a script on server in the deployment synchronously
       def run_script(friendly_name, server, options = nil)
         run_script_on_set(friendly_name, server, true, options)
       end
-  
+
       # Run a script on server in the deployment asynchronously
       def launch_script(friendly_name, server, options = nil)
         raise "No script registered with friendly_name #{friendly_name} for server #{server.inspect}" unless script_to_run?(friendly_name, server)
         transaction { server.run_executable(@scripts_to_run[resource_id(server.server_template_href)][friendly_name], options) }
       end
-  
+
       # Call run_script_on_set with out-of-order params passed in as a hash
       def run_script!(friendly_name, hash = {})
         hash['servers'] = @servers unless hash['servers']
         hash['wait'] = true unless hash['wait']
         run_script_on_set(friendly_name, hash['servers'], hash['wait'], hash['options'])
       end
-  
+
       # Returns false or true if a script with friendly_name has been registered for all or just one server
       def script_to_run?(friendly_name, server = nil)
         if server.nil? #check for all
@@ -371,7 +371,7 @@ module VirtualMonkey
         end
         ret
       end
-  
+
       # probe executes a shell command over ssh to a set of servers is provides the following functionality:
       def probe(set, command, &block)
         # run command on set over ssh
@@ -391,7 +391,7 @@ module VirtualMonkey
         }
         result_status
       end
-      
+
       # Detect operating system on each server and stuff the corresponding values for platform into the servers params (for temp storage only)
       def detect_os
         @servers.each do |server|
@@ -410,7 +410,7 @@ module VirtualMonkey
           end
         end
       end
-      
+
       # Assumes the host machine is in the cloud, uses the toolbox functions to grab the IP address of this
       # 'tester server' eg. used for the input variation MASTER_DB_DNSNAME
       def get_tester_ip_addr
@@ -419,15 +419,15 @@ module VirtualMonkey
         else
           ip = "127.0.0.1"
         end
-        return "text:#{ip}" 
+        return "text:#{ip}"
       end
-      
+
       # Log rotation
       def force_log_rotation(server)
         response = probe(server, 'logrotate -f /etc/logrotate.conf')
         raise "Logrotate restart failed" unless response
       end
-      
+
       def log_check(server, logfile)
         response = nil
         count = 0
@@ -439,8 +439,8 @@ module VirtualMonkey
           sleep 10
         end
         raise "Log file does not exist: #{logfile}" unless response
-      end   
-  
+      end
+
       # Checks that monitoring is enabled on all servers in the deployment.  Will raise an error if monitoring is not enabled.
       def check_monitoring
         @servers.each do |server|
@@ -484,9 +484,9 @@ module VirtualMonkey
           return false # Exception Not Handled
         end
       end
-  
-  
-  
+
+
+
   # TODO - we do not know what the RS_INSTANCE_ID available to the testing.
   # For now we are checking at a high level that the services are working
   # and then assume that the config file changes done during start are
@@ -512,7 +512,7 @@ module VirtualMonkey
           check_monitoring
         end
       end
-  
+
   # TODO there will be other files that need compares et al.  Create a list
   # of them and abstarct the tests
       # Copy configuration files into some location for usage after start
@@ -522,7 +522,7 @@ module VirtualMonkey
         probe(server, 'cp /etc/postfix/main.cf /root/start_stop_backup/.')
         probe(server, 'cp /etc/syslog-ng/syslog-ng.conf /root/start_stop_backup/.')
       end
-  
+
       # Diff the new config file with the saved one and check that the only
       # line that is different is the one that has the mydestination change
       def test_mail_config(server)
@@ -530,17 +530,17 @@ module VirtualMonkey
   # This is lame - assuming if the file is modified then it's okay
           raise "ERROR: postfix main.cf configuration file did not change when restarted" unless res
       end
-      
+
       def test_syslog_config(server)
         res = probe(server, 'diff /etc/syslog-ng/syslog-ng.conf /root/start_stop_backup/syslog-ng.conf')
   # This is lame - assuming if the file is modified then it's okay
         raise "ERROR: syslog-ng configuration file did not change when restarted" unless res
       end
-      
+
       def run_simple_checks
         @servers.each { |s| run_simple_check(s) }
       end
-      
+
       # this is where ALL the generic application server checks live, this could get rather long but for now it's a single method with a sequence of checks
       def run_simple_check(server)
         test_mail_config(server)
