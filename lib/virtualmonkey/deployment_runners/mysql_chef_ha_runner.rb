29b70748dce56a5ff6261d4d264d5e0ed3c2f3de
Erik Kargard <erik@rightscale.com>
2011-09-29 03:15:08 +0000

    Wait for snapshots was checking for wrong status return.

--- lib/virtualmonkey/deployment_runners/mysql_chef_ha_runner.rb
+++ lib/virtualmonkey/deployment_runners/mysql_chef_ha_runner.rb
@@ -7,7 +7,7 @@ module VirtualMonkey
       include VirtualMonkey::Mixin::Chef
       attr_accessor :scripts_to_run
       attr_accessor :db_ebs_prefix
-  
+
       # pass in the instance server on which we want to check the backup on
       def wait_for_snapshots(server)
         done = false
@@ -26,7 +26,7 @@ module VirtualMonkey
               timeout=1500
               step=10
               snapshots =find_snapshots
-              status = snapshots.map { |x| x.aws_status } 
+              status = snapshots.map { |x| x.aws_status }
               break unless status.include?("pending")
               sleep step
               timeout -= step
@@ -34,7 +34,7 @@ module VirtualMonkey
         end
         raise "FATAL: timed out waiting for all snapshots in lineage #{@lineage} to complete" if timeout <= 0
       end
-  
+
     end
   end
 end
