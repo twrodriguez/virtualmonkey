903d041234c48b45bee234205d94ad94b171fd47
twrodriguez <timothy@rightscale.com>
2011-10-14 23:48:06 -0700

    bugfixes

--- lib/virtualmonkey/command/common_logic.rb
+++ lib/virtualmonkey/command/common_logic.rb
@@ -276,7 +276,7 @@ module VirtualMonkey
       @@command_flags["#{@@command}"].each { |flag|
         if @@options["#{flag}_given".to_sym]
           actual_flag = "--#{flag.to_s.gsub(/_/, "-")}"
-          cmd_line += " --#{actual_flag} #{[@@options[flag]].flatten.map { |arg| arg.inspect }.join(" ")}"
+          cmd_line += " #{actual_flag} #{[@@options[flag]].flatten.map { |arg| arg.inspect }.join(" ")}"
         end
       }
       return cmd_line
--- lib/virtualmonkey/runner_mixins/command_hooks.rb
+++ lib/virtualmonkey/runner_mixins/command_hooks.rb
@@ -6,37 +6,42 @@ module VirtualMonkey
       #
 
       def before_create(*args, &block)
-        @@before_create ||= []
-        @@before_create |= args
-        @@before_create << block if block_given?
-        @@before_create
+        @@before_create ||= {}
+        @@before_create[self.to_s] ||= []
+        @@before_create[self.to_s] |= args
+        @@before_create[self.to_s] << block if block_given?
+        @@before_create[self.to_s]
       end
 
       def before_destroy(*args, &block)
-        @@before_destroy ||= []
-        @@before_destroy |= args
-        @@before_destroy << block if block_given?
-        @@before_destroy
+        @@before_destroy ||= {}
+        @@before_destroy[self.to_s] ||= []
+        @@before_destroy[self.to_s] |= args
+        @@before_destroy[self.to_s] << block if block_given?
+        @@before_destroy[self.to_s]
       end
 
       def after_create(*args, &block)
-        @@after_create ||= []
-        @@after_create |= args
-        @@after_create << block if block_given?
-        @@after_create
+        @@after_create ||= {}
+        @@after_create[self.to_s] ||= []
+        @@after_create[self.to_s] |= args
+        @@after_create[self.to_s] << block if block_given?
+        @@after_create[self.to_s]
       end
 
       def after_destroy(*args, &block)
-        @@after_destroy ||= []
-        @@after_destroy |= args
-        @@after_destroy << block if block_given?
-        @@after_destroy
+        @@after_destroy ||= {}
+        @@after_destroy[self.to_s] ||= []
+        @@after_destroy[self.to_s] |= args
+        @@after_destroy[self.to_s] << block if block_given?
+        @@after_destroy[self.to_s]
       end
 
       def description(desc="")
-        @@description ||= desc
-        raise "FATAL: Description must be a string" unless @@description.is_a?(String)
-        @@description
+        @@description ||= {}
+        @@description[self.to_s] ||= desc
+        raise "FATAL: Description must be a string" unless @@description[self.to_s].is_a?(String)
+        @@description[self.to_s]
       end
 
       def assert_integrity!
